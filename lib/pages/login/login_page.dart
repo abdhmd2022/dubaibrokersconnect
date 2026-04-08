3d8d9c2924c57b5f769f13c0e31fac12d55ce089
saadan <saadan@ca-eim.com>
2026-04-08 12:23:22 +0400

    "commit v1.0"

--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -105,6 +105,7 @@ class _LoginPageState extends State<LoginPage> {
         final userData = data['data']['user'];
 
 
+
         final isVerified = userData['broker']?['isVerified'] ?? false;
         await prefs.setString('user_id', userData['id'].toString());
         await prefs.setBool('isVerified',isVerified );
